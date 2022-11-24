0c3d3d51d2d22e4d25e90eab49bb118f22ea37c5
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2022-11-24 20:59:13 +0530

    LocatorsSelenium

--- src/SelIntroduction.java
+++ src/SelIntroduction.java
@@ -1,13 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Arrays;
 
 public class SelIntroduction {
 
 	public static void main(String[] args) {
 		//from selenium version 4.6.0(selenium manager) there is no need to setProperty of chromedriver, hence commented the below line
-	    //System.setProperty("webdriver.chrome.driver","/Users/vinuthar/eclipse-workspace/chromedriver");
+	    //System.setProperty("webdriver.chrome.driver","/Users/xxx/eclipse-workspace/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/");
