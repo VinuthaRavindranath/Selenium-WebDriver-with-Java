6c13eed8a12d6e1626922d9751c148189d17b770
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2022-11-28 21:41:10 +0530

    AutomatingWebElements

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,20 +1,25 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.time.Duration;
 
-public class DynamicDropdown {
+public class Calendar {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-        driver.findElement(By.cssSelector("input[id*=\"originStation1_CTX\"]")).click();
-        driver.findElement(By.cssSelector("a[value=\"BLR\"]")).click();
-      //  driver.findElement(By.xpath("(//a[@value=\"GOI\"])[2]")).click();
-        driver.findElement(By.xpath("//div[@id=\"glsctl00_mainContent_ddl_destinationStation1_CTNR\"]//a[@value=\"GOI\"]")).click();
-        driver.close();
+        driver.findElement(By.cssSelector("input[id*=\"rbtnl_Trip_1\"]")).click();
+
+        if ( driver.findElement(By.className("picker-second")).getAttribute("style").contains("1") ){
+            Assert.assertTrue(true);
+            driver.findElement(By.cssSelector("input[id*=\"view_date2\"]")).click();
+        }
+        else {
+            Assert.assertTrue(false);
+        }
     }
 }
