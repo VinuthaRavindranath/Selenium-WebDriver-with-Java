00a3fd18695bd6155b4484a52aa3a699ead154bf
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2022-12-01 21:42:01 +0530

    implictWait

--- src/EcommerceWebsite/AddMultipleProductsToCart.java
+++ src/EcommerceWebsite/AddMultipleProductsToCart.java
@@ -4,19 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
 public class AddMultipleProductsToCart {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        //driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-        String ListOfProducts[]={"Cucumber","Brocolli","Beetroot"};
+    public static void openBrowser( WebDriver driver){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+    }
+
+    public static void addItemsToCart(WebDriver driver, String[] ListOfProducts){
         List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
 
         int productsSize=products.size();
@@ -27,15 +29,33 @@ public class AddMultipleProductsToCart {
             String productTitles=productNames[0].trim();
             List productsList = Arrays.asList(ListOfProducts);
             int j=0;
-                if (productsList.contains(productTitles)){
-                    j++;
-                    driver.findElements(By.xpath("//button[text()=\"ADD TO CART\"]")).get(i).click();
-                    if (j==ListOfProducts.length){
-                        break;
-                    }
+            if (productsList.contains(productTitles)){
+                j++;
+                driver.findElements(By.cssSelector("div.product-action > button")).get(i).click();
+                if (j==ListOfProducts.length){
+                    break;
                 }
+            }
 
         }
+    }
+
+    public static void checkout(WebDriver driver){
+        driver.findElement(By.cssSelector("img[alt=\"Cart\"]")).click();
+        driver.findElement(By.xpath("//button[text()=\"PROCEED TO CHECKOUT\"]")).click();
+
+        driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("promoBtn")).click();
+        Assert.assertEquals(driver.findElement(By.className("promoInfo")).getText(),"Code applied ..!");
+        driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        openBrowser(driver);
+        String ListOfProducts[]={"Cucumber","Water Melon","Beetroot","Walnuts"};
+        addItemsToCart(driver, ListOfProducts);
+        checkout(driver);
+
 
     }
 }
