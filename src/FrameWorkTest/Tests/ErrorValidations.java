a3a4715cce643c40b959f5a018dcc14991945155
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2022-12-23 22:01:02 +0530

    FrameworkDevelopment

--- src/FrameWorkTest/Tests/ErrorValidations.java
+++ src/FrameWorkTest/Tests/ErrorValidations.java
@@ -2,18 +2,17 @@ package FrameWorkTest.Tests;
 
 
 import FrameWorkTest.TestComponents.BaseTest;
+import FrameWorkTest.TestComponents.Retry;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 
 public class ErrorValidations extends BaseTest {
-    @Test
+    @Test(retryAnalyzer = Retry.class)
     public void loginWithIncorrectCredentials(){
         loginPage.login("vinuthatest27@gmail.com", "test@12356");
         String error = loginPage.getErrorMessage();
-        Assert.assertEquals(error,"Incorrect email or password.");
-
-
+        Assert.assertEquals(error,"Incorrect emai or password.");
     }
 }
